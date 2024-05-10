b52ad1f17fa692a77eb9c70b02e351dd00ad9272
Abhiraj Basnet <abhirajbasnet@Abhirajs-MacBook-Pro.local>
2024-05-10 13:52:23 +1000

    working in the project

--- script.js
+++ script.js
@@ -99,4 +99,20 @@ const renderSongs = (array) => {
           
     </li>`
   }).join("");
+
+
 }
+
+const setPlayerDisplay = () =>{
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  condition ? exprIfTrue : exprIfFalse
+  playingSong.textContent = currentTitle ? currentTitle : "" ;
+  songArtist.textContent = currentArtist ? currentArtist : "" ;
+
+}
+ 04 
